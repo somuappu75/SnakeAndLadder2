f03b69024224da967eea2e9797c6874f123f0021
Lenovo <somusp75@gmail.com>
2022-01-09 21:29:13 +0530

    Add project files.

---
+++ SnakeAndLadder2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnakeAndLadder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Set the Foreground color to blue
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            // Display current Foreground color
+            Console.WriteLine("Changed Foreground Color: {0}",
+                                    Console.ForegroundColor);
+            PlayerPositin.Position();
+
+        }
+    }
+}
